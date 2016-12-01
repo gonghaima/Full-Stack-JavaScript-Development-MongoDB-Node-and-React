f30da66135cb5c04fab78ff51973e493a034358d
gonghaima@hotmail.com <5418854188>
2016-12-02 02:00:23 +1300

    Component state

--- public/bundle.js
+++ public/bundle.js
@@ -57,61 +57,13 @@
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
+	var _App = __webpack_require__(/*! ./components/App */ 178);
+	
+	var _App2 = _interopRequireDefault(_App);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	// const Header = (message) => {
-	//     return (
-	//         <h2>
-	//             Hello React with JSX COM {message}
-	//         </h2>
-	//     );
-	// };
-	
-	// Header.propTypes = {
-	//     message: React.PropTypes.string.isRequired
-	// };
-	
-	// const App = (props) => {
-	//     return (
-	//         <div>
-	//             <Header message="Naming Contests"/>
-	//             <div>
-	//                 ...**************
-	//      </div>
-	//         </div>
-	//     );
-	// };
-	
-	
-	// ReactDOM.render(
-	//     <App />,
-	//     document.getElementById('root')
-	// );
-	
-	
-	var Header = function Header(_ref) {
-	    var message = _ref.message;
-	
-	    return _react2.default.createElement(
-	        'h2',
-	        { className: 'Header text-center' },
-	        message
-	    );
-	};
-	
-	Header.propTypes = {
-	    message: _react2.default.PropTypes.string
-	};
-	var App = function App() {
-	    return _react2.default.createElement(
-	        'div',
-	        { className: 'App' },
-	        _react2.default.createElement(Header, { message: 'naming contests' }),
-	        _react2.default.createElement('div', null)
-	    );
-	};
-	
-	_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('root'));
+	_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('root'));
 
 /***/ },
 /* 1 */
@@ -22092,6 +22044,120 @@
 	
 	module.exports = ReactDOMInvalidARIAHook;
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! ./~/process/browser.js */ 3)))
+
+/***/ },
+/* 178 */
+/*!*******************************!*\
+  !*** ./src/components/App.js ***!
+  \*******************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _Header = __webpack_require__(/*! ./Header */ 179);
+	
+	var _Header2 = _interopRequireDefault(_Header);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var App = function (_React$Component) {
+	    _inherits(App, _React$Component);
+	
+	    function App() {
+	        var _ref;
+	
+	        var _temp, _this, _ret;
+	
+	        _classCallCheck(this, App);
+	
+	        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	            args[_key] = arguments[_key];
+	        }
+	
+	        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = App.__proto__ || Object.getPrototypeOf(App)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+	            pageHeader: 'Naming Contests'
+	        }, _temp), _possibleConstructorReturn(_this, _ret);
+	    }
+	    // constructor(props) {
+	    //     super(props);
+	    //     this.state = { test: 42 };
+	    // }
+	
+	    _createClass(App, [{
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement(
+	                'div',
+	                { className: 'App' },
+	                _react2.default.createElement(_Header2.default, { message: this.state.pageHeader }),
+	                _react2.default.createElement(
+	                    'div',
+	                    null,
+	                    '...'
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return App;
+	}(_react2.default.Component);
+	
+	exports.default = App;
+
+/***/ },
+/* 179 */
+/*!**********************************!*\
+  !*** ./src/components/Header.js ***!
+  \**********************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(/*! react-dom */ 32);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var Header = function Header(_ref) {
+	    var message = _ref.message;
+	
+	    return _react2.default.createElement(
+	        'h2',
+	        { className: 'Header text-center' },
+	        message
+	    );
+	};
+	
+	Header.propTypes = {
+	    message: _react2.default.PropTypes.string
+	};
+	
+	exports.default = Header;
 
 /***/ }
 /******/ ]);
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Header from './Header';
 
-const App = () => {
-    return (
-  
-        <div className="App">
-            <Header message="naming contests"/>
-            <div>
-                
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { test: 42 };
+    // }
+
+    state = {
+        pageHeader:'Naming Contests'
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader} />
+                <div>
+                    ...
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
+}
 
 export default App;
