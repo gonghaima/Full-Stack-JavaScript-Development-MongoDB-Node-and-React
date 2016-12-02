22912ed298166a89af3d8f48ff23b1a2b1cbc39c
gonghaima@hotmail.com <5418854188>
2016-12-03 00:55:07 +1300

    Working with data - node js data api

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// import http from 'http';
-
-// const server=http.createServer();
-
-// server.listen(8080);
-
-// server.on('request', (req, res)=>{
-//     res.write('Hello HTTP!\n');
-
-//     setTimeout(()=>{
-//         res.write('I can stream!\n');
-//         res.end();
-//     },3000);
-// });
-
 import config from './config';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 import express from 'express';
 const server = express();
+
+server.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')    
+}));
 
 server.set('view engine', 'ejs');
 
@@ -30,37 +22,6 @@ server.get('/',(req, res)=>{
 server.use('/api',apiRouter);
 server.use(express.static('public'));
 
-// server.get('/about.html',(req, res)=>{
-//     fs.readFile('./ab.html', (err,data)=>{
-//         res.send(data.toString());
-//     });
-// });
-
 server.listen(config.port, () => {
     console.info('Express listening on port ', config.port);
 });
-// import apiRouter from './api';
-// // import fs from 'fs';
-
-// const server = express();
-// server.set('view engine','ejs');
-
-// server.get('/',(req, res)=>{
-//     // res.send('Hello Express');
-//     res.render('index',{
-//         content:'Hello Express and ejs! <em>EJS</em>'
-//     });
-// });
-
-// server.use('/api',apiRouter);
-// server.use(express.static('public'));
-
-// // server.get('/about.html',(req, res)=>{
-// //     fs.readFile('./about.html',(err,data)=>{
-// //         res.send(data.toString());
-// //     });
-// // });
-
-// server.listen(config.port, ()=>{
-//     console.info('Express listening on port ', config.port);
-// });
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
+import ContestPreview from './ContestPreview'
+
 
 class App extends React.Component {
     // constructor(props) {
@@ -8,23 +11,33 @@ class App extends React.Component {
     // }
 
     state = {
-        pageHeader:'Naming Contests'
+        pageHeader: 'Naming Contests',
+        contests: []
     };
 
     componentDidMount() {
-        
+        axios.get('/api/contests')
+            .then(resp => {
+                console.log(resp.data.contests);
+                this.setState({
+                    contests: resp.data.contests
+                });
+            })
+            .catch(console.error);
     }
-    
+
     componentWillUnmount() {
-        
+
     }
-    
+
     render() {
         return (
             <div className="App">
                 <Header message={this.state.pageHeader} />
                 <div>
-                    ...
+                    {this.state.contests.map(contest =>
+                        <ContestPreview key={contest.id} {...contest} />
+                    )}
                 </div>
             </div>
         );
