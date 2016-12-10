1ede8ea7f23c09905fe7afbcd0fe9144f2555619
gonghaima@hotmail.com <5418854188>
2016-12-10 19:14:44 +1300

    MongoDB - Reading data from MongoDB

--- api/index.js
+++ api/index.js
@@ -1,24 +1,42 @@
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, db) => {
+  assert.equal(null, err);
+
+  mdb = db;
+});
 
 const router = express.Router();
 
-const contests=data.contests.reduce((obj,contest)=>{
-    obj[contest.id]=contest;
-    return obj;
-},{});
+router.get('/contests', (req, res) => {
+  let contests = {};
+  mdb.collection('contests').find({})
+     .project({
+       id: 1,
+       categoryName: 1,
+       contestName: 1
+     })
+     .each((err, contest) => {
+       assert.equal(null, err);
 
-router.get('/contests', (req,res)=>{
-    res.send({
-        contests: contests
-    });
+       if (!contest) { // no more contests
+         res.send({ contests });
+         return;
+       }
+
+       contests[contest.id] = contest;
+     });
 });
 
-router.get('/contests/:contestId', (req,res)=>{
-    let contest = contests[req.params.contestId];
-    contest.description="sdfafdsafsfwfdsf332dsf";
-
-    res.send(contest);
+router.get('/contests/:contestId', (req, res) => {
+  mdb.collection('contests')
+     .findOne({ id: Number(req.params.contestId) })
+     .then(contest => res.send(contest))
+     .catch(console.error);
 });
 
 export default router;
--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const env=process.env;
 export const nodeEnv=env.NODE_ENV || 'development';
 
 export default {
+    mongodbUri: 'mongodb://localhost:27017/test',
     port: env.PORT || 8080,
     host: env.HOST || 'localhost',
     get serverUrl(){
