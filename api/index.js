f6affb3dd8803fc27c958bdf955f6059e03317ba
gonghaima@hotmail.com <5418854188>
2016-12-10 22:12:46 +1300

    MongoDB - api to fetch a list of names

--- api/index.js
+++ api/index.js
@@ -5,38 +5,54 @@ import config from '../config';
 
 let mdb;
 MongoClient.connect(config.mongodbUri, (err, db) => {
-  assert.equal(null, err);
+    assert.equal(null, err);
 
-  mdb = db;
+    mdb = db;
 });
 
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
-  let contests = {};
-  mdb.collection('contests').find({})
-     .project({
-       id: 1,
-       categoryName: 1,
-       contestName: 1
-     })
-     .each((err, contest) => {
-       assert.equal(null, err);
+    let contests = {};
+    mdb.collection('contests').find({})
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1
+        })
+        .each((err, contest) => {
+            assert.equal(null, err);
 
-       if (!contest) { // no more contests
-         res.send({ contests });
-         return;
-       }
+            if (!contest) { // no more contests
+                res.send({ contests });
+                return;
+            }
 
-       contests[contest.id] = contest;
-     });
+            contests[contest.id] = contest;
+        });
+});
+
+router.get('/names/:nameIds', (req, res) => {
+    const nameIds = req.params.nameIds.split(',').map(Number);
+    let names = {};
+    mdb.collection('names').find({id:{$in:nameIds}})
+    .each((err, name) => {
+        assert.equal(null, err);
+
+        if (!name) { // no more names
+            res.send({ names });
+            return;
+        }
+
+        names[name.id] = name;
+    });
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  mdb.collection('contests')
-     .findOne({ id: Number(req.params.contestId) })
-     .then(contest => res.send(contest))
-     .catch(console.error);
+    mdb.collection('contests')
+        .findOne({ id: Number(req.params.contestId) })
+        .then(contest => res.send(contest))
+        .catch(console.error);
 });
 
 export default router;
